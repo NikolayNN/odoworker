bbc807a04de3c9ef38ab2a12b01ce0480f444a53
nikolaynn <kolya.by@mail.ru>
2021-02-23 15:32:52 +0300

    fix script

--- src/test/resources/sql/init-temp-odo.sql
+++ src/test/resources/sql/init-temp-odo.sql
@@ -4,5 +4,5 @@ CREATE TABLE unit_odo_temp (
     unit_id integer,
     last_processed_datetime timestamp,
     last_processed_message_id integer,
-    CONSTRAINT command_pk PRIMARY KEY ("unit_id")
+    CONSTRAINT unit_odo_temp_pk PRIMARY KEY ("unit_id")
 )
